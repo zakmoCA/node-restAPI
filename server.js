9325902795f747e648405cbae29131ea996afe39
zakmoCA <zakeriyaamohamed@gmail.com>
2023-09-30 19:23:04 +1000

    clean up routing

--- server.js
+++ server.js
@@ -16,30 +16,22 @@ const routes = {
   '/api/products/:id': {
     'GET': (req, res, id) => getProduct(req, res, id),
     'DELETE': (req, res, id) => deleteThisProduct(req, res, id),
-    'PUT': (req, res, id) => getProduct(req, res, id),
+    'PUT': (req, res, id) => updateProduct(req, res, id),
   }
 }
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/api/products' && req.method === 'GET') {
-    getProducts(req, res)
-  } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'GET') {
-      const id = req.url.split('/')[3]
-      getProduct(req, res, id)
-  } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'DELETE') {
-      const id = req.url.split('/')[3]
-      deleteThisProduct(req, res, id)
-  } else if (req.url === '/api/products' && req.method === 'POST') {
-      createProduct(req, res)
-  } else if (req.url.match(/\/api\/products\/([0-9]+)/) && req.method === 'PUT') {
-      const id = req.url.split('/')[3]
-      updateProduct(req, res, id)
+  const routeKey = req.url.match(/\/api\/products\/([0-9]+)/) ? '/api/products/:id' : req.url
+
+  if (routes[routeKey] && routes[routeKey][req.method]) {
+    const handler = routes[routeKey][req.method]
+    const id = routeKey.includes(':id') ? extractIdFromUrl(req.url) : null
+    handler(req, res, id)
   } else {
-      res.writeHead(404, {'Content-Type': 'application/json'})
-      res.end(JSON.stringify({ message: 'Route Not Found' }))
+    res.writeHead(404, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({ message: 'Route Not Found' }))
   }
 })
-
 
 
 const PORT = process.env.PORT || 6000
